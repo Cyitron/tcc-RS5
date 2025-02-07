29bf80a801853ebc74fbefaadd6a2463dad8289d
Gabriel Lencina <gabriel.lencina@paxos.inf.pucrs.br>
2025-02-07 18:45:48 -0300

    tentando corrigir o problema

--- app/hello/src/hello.c
+++ app/hello/src/hello.c
@@ -1,12 +1,17 @@
 #include "../../common/include/riscv-csr.h"
 #include <stdio.h>
+#include <inttypes.h>
 
 int main()
 {
-    printf("\nHello World!\n");
-
     uint64_t minstret_value = csr_read_minstret();
-    printf("minstret: %llu\n", minstret_value);
+    printf("Hello World!\n");
+
+    // Teste imprimindo um valor fixo
+    printf("Teste de uint64_t: %" PRIu64 "\n", (uint64_t) 1234567890);
+
+    // Agora imprime o minstret
+    printf("minstret: %" PRIu64 "\n", minstret_value);
 
     return 0;
 }
