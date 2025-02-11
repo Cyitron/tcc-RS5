5e60dd6e2e4a3d9c5a2d9a1adf73ac770a3620a9
Gabriel Lencina <gabriel.lencina@paxos.inf.pucrs.br>
2025-02-11 17:16:18 -0300

    test pass 2/2 minor changes2

--- app/hello/src/hello.c
+++ app/hello/src/hello.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 static inline void print_binary(uint32_t num) {
-    for (int i = 32; i >= 0; i--) {
+    for (int i = 31; i >= 0; i--) {
         // Verifica se o bit i está setado e imprime '1' ou '0'
         putchar((num & (1u << i)) ? '1' : '0');
     }
@@ -16,27 +16,27 @@ int main()
     //uint64_t csr_read_mcycle = csr_read_minstret();
     printf("Hello World!\n");
 
-    uint32_t data = 0xFFFFFFFF;
+    // uint32_t data = 0xFFFFFFFF;
     
-    // Loop que continua enquanto 'data' for diferente de 0
-    while (data != 0) {
-        // Imprime a representação binária de 'data'
-        print_binary(data);
+    // // Loop que continua enquanto 'data' for diferente de 0
+    // while (data != 0) {
+    //     // Imprime a representação binária de 'data'
+    //     print_binary(data);
 
-        // Desloca os bits para a esquerda (inserindo 0 no bit menos significativo)
-        data = data << 1;
-    }
+    //     // Desloca os bits para a esquerda (inserindo 0 no bit menos significativo)
+    //     data = data << 1;
+    // }
 
-    // Lê o contador de ciclos (mcycle)
-    uint32_t mcycle_value;
-    mcycle_value = csr_read_mcycle();
+    // // Lê o contador de ciclos (mcycle)
+    // uint32_t mcycle_value;
+    // mcycle_value = csr_read_mcycle();
 
-    uint_xlen_t read_cycle_value;
-    read_cycle_value = csr_read_cycle();
+    // uint_xlen_t read_cycle_value;
+    // read_cycle_value = csr_read_cycle();
     
-    // Imprime o valor lido.
-    printf("\nmcycle: %lu", mcycle_value);
-    printf("\nread cycle: %lu", read_cycle_value);
+    // // Imprime o valor lido.
+    // printf("\nmcycle: %lu", mcycle_value);
+    // printf("\nread cycle: %lu", read_cycle_value);
 
     return 0;
 }
