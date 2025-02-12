a53a08b4bb51d89a18dc586ea422387f093ebb66
Gabriel Lencina <gabriel.lencina@paxos.inf.pucrs.br>
2025-02-12 00:17:49 -0300

    all the performance counter

--- app/hello/src/hello.c
+++ app/hello/src/hello.c
@@ -3,6 +3,19 @@
 #include <inttypes.h>
 #include <stdint.h>
 
+uint32_t mcycle_value;
+uint32_t m_inst_retired;
+uint_xlen_t mhpmcounter3;
+uint_xlen_t mhpmcounter4;
+uint_xlen_t mhpmcounter31;
+uint_xlen_t hpmcounter3;
+uint_xlen_t hpmcounter4;
+uint_xlen_t hpmcounter31;
+uint_xlen_t mpec;
+uint_xlen_t upec;
+uint_xlen_t read_cycle_value;
+uint_xlen_t instret;
+
 static inline void print_binary(uint32_t num) {
     for (int i = 31; i >= 0; i--) {
         // Verifica se o bit i está setado e imprime '1' ou '0'
@@ -11,13 +24,9 @@ static inline void print_binary(uint32_t num) {
     putchar('\n');
 }
 
-static inline void nop(){
-    __asm__ volatile("nop");
-}
-
 int main()
 {
-    nop();
+    //nop();
 
     //printf("Hello World!\n");
 
@@ -33,15 +42,51 @@ int main()
     // }
 
     // Lê o contador de ciclos (mcycle)
-    uint32_t mcycle_value;
     mcycle_value = csr_read_mcycle();
 
-    uint_xlen_t read_cycle_value;
+    //funções executudas com sucesso pela CPU (retired)
+    m_inst_retired = csr_read_minstret();
+
+    // Machine performance-monitoring counter
+    mhpmcounter3 = csr_read_mhpmcounter3();
+    mhpmcounter4 = csr_read_mhpmcounter4();
+    mhpmcounter31 = csr_read_mhpmcounter31();
+
+    // Machine Exception Program Counter
+    mpec = csr_read_mepc();
+
+    // User Exception Program Counter
+    upec =  csr_read_uepc();
+
+    // Cycle counter for RDCYCLE instruction
     read_cycle_value = csr_read_cycle();
-    
+
+    // Instructions-retired counter for RDINSTRET instruction
+    instret = csr_read_instret();
+
+    // Performance-monitoring counter
+    hpmcounter3 = csr_read_hpmcounter3();
+    hpmcounter4 = csr_read_hpmcounter4();
+    hpmcounter31 = csr_read_hpmcounter31();
+
     // Imprime o valor lido.
     printf("\nmcycle: %lu", mcycle_value);
-    printf("\nread cycle: %lu", read_cycle_value);
+    printf("\nRD cycle: %lu", read_cycle_value);
+    printf("\nminstret: %lu", m_inst_retired);
+    printf("\nmhpmcounter3: %lu", mhpmcounter3);
+    printf("\nmhpmcounter4: %lu", mhpmcounter4);
+    printf("\nmhpmcounter31: %lu", mhpmcounter31);
+    printf("\nmepc: %lu", mpec);
+    printf("\nuepc: %lu", upec);
+    printf("\ninstret: %lu", instret);
+    printf("\nhpmcounter3: %lu", hpmcounter3);
+    printf("\nhpmcounter4: %lu", hpmcounter4);
+    printf("\nhpmcounter31: %lu", hpmcounter31);
 
     return 0;
 }
+
+//==========================//
+// static inline void nop(){
+//     __asm__ volatile("nop");
+// }
